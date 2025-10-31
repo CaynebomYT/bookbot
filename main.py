bcd1c2b2fb0e32bc384ce5b7dc26f067f2e97439
CaynebombYT <caynebomb@gmail.com>
2025-10-31 15:03:14 -0400

    report 1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import sort_by_occurence
+import sys
 
 def get_books_text(file_path):
     with open(file_path) as f:
@@ -11,7 +13,19 @@ def main():
     text = get_books_text("./books/frankenstein.txt")
     word_count = get_word_count(text)
     character_count = get_character_count(text)
+    character_count = sort_by_occurence(character_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print("Found", word_count, "total words")
+    print("--------- Character Count -------")
+    for character in character_count:
+        is_letter = character["char"]
+        if is_letter.isalpha() == True:
+            print(f"{is_letter}:", character["num"])
+    print("============= END ===============")
     
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,17 @@ def get_character_count(some_text):
             else:
                 character_count[character] = 1
     return character_count
+
+def sort_by_occurence(some_dictionary):
+    sorted_list = []
+    
+    for key, value in some_dictionary.items():
+        sorted_list.append({"char": key, "num": value})
+    def sort_on(item):
+        return item["num"]
+    sorted_list.sort(reverse=True, key=sort_on)   
+    return sorted_list    
+        
+    
+    
+        
